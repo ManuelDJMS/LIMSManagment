b7684657d92f8af9fb33cadfc7faa9f263b39304
PaauRF <paaurodriiguez@gmail.com>
2019-04-11 09:54:26 -0500

    Cambio de conexión
    Conexion a MetAs_Live

--- LIMSManagment/Conexiones/Metas2019.vb
+++ LIMSManagment/Conexiones/Metas2019.vb
@@ -3,11 +3,12 @@ Module Metas2019
     Public conexion2019 As SqlConnection              ''Base de datos METASINF-2019
     Public comando2019 As SqlCommand
     Public lector2019 As SqlDataReader
-    Public cnt As String = "Data Source=PC93-L;Initial Catalog=MetAs_Live; Integrated Security=true"
+    Public cnt As String = "Data Source=DATABASESERVER\COMPAC; Initial Catalog=MetAs_Live; User Id=sa; Password=Contpaq1; Integrated Security=false"
     Sub MetodoMetasInf2019()
         Try
-            conexion2019 = New SqlConnection("Data Source=PC93-L;Initial Catalog=MetAs_Live; Integrated Security=true")
+            conexion2019 = New SqlConnection("Data Source=DATABASESERVER\COMPAC;Initial Catalog=MetAs_Live; User Id=sa; Password=Contpaq1; Integrated Security=false")
             conexion2019.Open()
+            MsgBox("Conectado a MetAs_Live")
         Catch ex As Exception
             MsgBox("No se pudo conectar a la base de datos" + ex.ToString)
         End Try
